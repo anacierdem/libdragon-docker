0a0561f540e8d7fd9ead1998c50e85fe050cb394
Ali Naci Erdem <anacierdem@gmail.com>
2019-12-06 23:27:51 +0300

    Update prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,4 @@
 module.exports = {
   singleQuote: true,
   trailingComma: 'es5',
-  overrides: [
-    {
-      files: ['**/*.{scss,less,css}'],
-      options: {
-        singleQuote: false,
-      },
-    },
-  ],
 };
