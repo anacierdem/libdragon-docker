b9115d1a6aa438216752123a979b49c648acaeea
Ali Naci Erdem <anacierdem@gmail.com>
2024-02-17 23:56:01 +0300

    build: enable SEA

--- pack.sh
+++ pack.sh
@@ -2,8 +2,6 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-npm run pack
 tar --transform='s/-linux//' -cvzf libdragon-linux-x86_64.tar.gz libdragon-linux
 tar --transform='s/-macos//' -cvzf libdragon-macos-x86_64.tar.gz libdragon-macos
-mv libdragon-win.exe libdragon.exe
 zip libdragon-win-x86_64.zip libdragon.exe
