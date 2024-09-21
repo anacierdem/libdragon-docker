17867e69a1bea517d8c3c5c1b6ea061117130a4b
Ali Naci Erdem <anacierdem@gmail.com>
2024-09-21 21:29:47 +0300

    fix: output stderr of a command failure for better context for submodule creation

--- modules/actions/init.js
+++ modules/actions/init.js
@@ -156,6 +156,9 @@ const autoVendor = async (info) => {
       if (!(e instanceof CommandError)) throw e;
       // We speculate this is caused by the user, so replace it with a more useful error message.
       e.message = `Unable to create submodule. If you have copied the executable in your project folder or you have a file named ${info.vendorDirectory}, removing it might solve this issue. Original error:\n${e.message}`;
+      if (e.out) {
+        e.message += `\n${e.out}`;
+      }
       throw e;
     }
 
